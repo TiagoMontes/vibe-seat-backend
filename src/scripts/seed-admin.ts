75e14355c6f39ef834814b3354b2cf4539ca3f10
TiagoMontes <tiagosantos6303@gmail.com>
2025-07-19 10:49:57 -0500

    feat(Lib & scripts): create a new lib to instance prisma and new script to create first user admin

---
+++ src/scripts/seed-admin.ts
@@ -0,0 +1,29 @@
+import { prisma } from "../lib/prisma";
+
+async function main() {
+  const roleAdmin = await prisma.role.upsert({
+    where: { name: 'admin' },
+    update: {},
+    create: { name: 'admin' },
+  });
+
+  const existingUser = await prisma.user.findFirst({
+    where: { username: 'admin@admin.com' },
+  });
+
+  if (!existingUser) {
+    const admin = await prisma.user.create({
+      data: {
+        username: 'Administrador',
+        password: 'admin123', 
+        roleId: roleAdmin.id,
+        status: 'approved',
+      },
+    });
+    console.log('✅ Admin criado com sucesso:', admin.username);
+  } else {
+    console.log('⚠️ Admin já existe:', existingUser.username);
+  }
+}
+
+main().finally(() => prisma.$disconnect());
